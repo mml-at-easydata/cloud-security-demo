3fe037cbc1b7c21d20c1ee1a0edb35cd8f60d46c
全 <1583090876@qq.com>
2020-11-03 19:52:50 +0800

    security cors

--- cloud-authorization-server/src/main/java/cn/lilq/cloudsecurity/cloudauthorizationserver/CloudAuthorizationServerApplication.java
+++ cloud-authorization-server/src/main/java/cn/lilq/cloudsecurity/cloudauthorizationserver/CloudAuthorizationServerApplication.java
@@ -8,6 +8,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 /**
  * @auther: Li Liangquan
  * @date: 2020/11/1 16:34
+ * http://127.0.0.1:7010/oauth/token
+ * http://127.0.0.1:8080/api/v1/authorization/oauth/token
  */
 
 @EnableResourceServer
--- cloud-authorization-server/src/main/java/cn/lilq/cloudsecurity/cloudauthorizationserver/security/WebSecurityConfigure.java
+++ cloud-authorization-server/src/main/java/cn/lilq/cloudsecurity/cloudauthorizationserver/security/WebSecurityConfigure.java
@@ -3,10 +3,16 @@ package cn.lilq.cloudsecurity.cloudauthorizationserver.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * @auther: Li Liangquan
@@ -26,6 +32,23 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
         return super.userDetailsServiceBean();
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 允许跨域访问
+        http.cors(Customizer.withDefaults());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");//修改为添加而不是设置，* 最好改为实际的需要，我这是非生产配置，所以粗暴了一点
+        configuration.addAllowedMethod("*");//修改为添加而不是设置
+        configuration.addAllowedHeader("*");//这里很重要，起码需要允许 Access-Control-Allow-Origin
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
